7f05159c7b2f2b65e432a667854d74eb23113cad
Cláudio Cezar <antoniosantos@cwi.com.br>
2020-11-23 21:19:26 -0300

    Initial Commit

---
+++ InstagramFirestore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramFirestore
+//
+//  Created by Cláudio Cezar on 23/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
