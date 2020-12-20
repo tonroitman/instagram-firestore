b7c96da816abc8482dbffd7cafa1d22dfdae9cae
Cláudio Cezar <claudiocezar16@gmail.com>
2020-12-20 12:56:39 -0300

    feat: User authentication

---
+++ InstagramFirestore/API/AuthService.swift
@@ -0,0 +1,44 @@
+//
+//  AuthService.swift
+//  InstagramFirestore
+//
+//  Created by Cláudio Cezar on 19/12/20.
+//
+
+import UIKit
+import Firebase
+
+struct AuthCredentials {
+    let email: String
+    let password: String
+    let fullname: String
+    let username: String
+    let profileImage: UIImage
+}
+
+struct AuthService {
+    static func logUserIn(withEmail email: String, password: String, completion: AuthDataResultCallback?) {
+        Auth.auth().signIn(withEmail: email, password: password, completion: completion)
+    }
+    
+    static func registerUser(withCredentials credentials: AuthCredentials, completion: @escaping(Error?) -> Void) {
+        ImageUploader.uploadImage(image: credentials.profileImage) { imageUrl in
+            Auth.auth().createUser(withEmail: credentials.email, password: credentials.password) { (result, error) in
+                if let error = error {
+                    print("DEBUG: Failed to register user \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let uid = result?.user.uid else { return }
+                
+                let data: [String: Any] = ["email": credentials.email,
+                                           "fullname": credentials.fullname,
+                                           "profileImageUrl": imageUrl,
+                                           "uid": uid,
+                                           "username": credentials.username]
+                
+                Firestore.firestore().collection("users").document(uid).setData(data, completion: completion)
+            }
+        }
+    }
+}
--- InstagramFirestore/Controller/FeedController.swift
+++ InstagramFirestore/Controller/FeedController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 private let reuserIdentifier = "Cell"
 
@@ -19,12 +20,28 @@ class FeedController: UICollectionViewController {
         configureUI()
     }
     
+    // MARK: - Actions
+    
+    @objc func handleLogout() {
+        do {
+            try Auth.auth().signOut()
+            let controller = LoginController()
+            let nav = UINavigationController(rootViewController: controller)
+            nav.modalPresentationStyle = .fullScreen
+            self.present(nav, animated: true, completion: nil)
+        } catch {
+            print("DEBUG: Failed to sign out")
+        }
+    }
+    
     // MARK: - Helpers
     
     func configureUI() {
         collectionView.backgroundColor = .white
         
         collectionView.register(FeedCell.self, forCellWithReuseIdentifier: reuserIdentifier)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
     }
     
 }
